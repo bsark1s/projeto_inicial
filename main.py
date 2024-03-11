e806c124355a2a64d69b5dade97687aafc19eacf
Mr Bruno Sarkis <mrbrunosarkis@gmail.com>
2024-03-11 18:37:42 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("JOrnada de dados")
+
+# Variáveis
+
+idade = 40
+
+# print
+
+print(idade)
+
+# soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Listas
+
+lista = [1,2,3,4,5]
+
+#Métodos
+lista_invertida = lista.reverse()
+
